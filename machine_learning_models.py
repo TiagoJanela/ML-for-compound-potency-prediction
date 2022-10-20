b41ee7bc8e0a3ac1553a3d241f3e1236ebfd1a65
Tiago Janela <tiago.janela@gmail.com>
2022-10-20 11:04:21 +0200

    Update machine_learning_models.py

--- machine_learning_models.py
+++ machine_learning_models.py
@@ -25,6 +25,21 @@ import warnings
 warnings.filterwarnings('ignore')
 pd.options.mode.chained_assignment = None
 
+def set_seeds(seed):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+
+def set_global_determinism(seed):
+    set_seeds(seed=seed)
+
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+    tf.config.threading.set_intra_op_parallelism_threads(1)
 
 class Dataset:
     def __init__(self, features: np.array, labels: np.array):
